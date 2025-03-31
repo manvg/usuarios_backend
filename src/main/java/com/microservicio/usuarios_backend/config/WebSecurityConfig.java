1d8131b4e3af8bf35d27da31bc6abbe9d3e04902
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-03-31 01:48:22 -0300

    Actualización url

--- src/main/java/com/microservicio/usuarios_backend/config/WebSecurityConfig.java
+++ src/main/java/com/microservicio/usuarios_backend/config/WebSecurityConfig.java
@@ -35,13 +35,14 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Cambia según la URL de tu frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:8084"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-
+    
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+    
 }
